846cd80d2d4719017668b3995ca0cddfb828b746
yangbian <yangbian@broada.com>
2016-12-15 18:37:31 +0800

    增加代码

---
+++ activity/src/main/java/com/yb/activity/Main.java
@@ -0,0 +1,5 @@
+package com.yb.activity;
+
+public class Main {
+
+}
---
+++ activity/src/test/java/activity/MainTest.java
@@ -0,0 +1,11 @@
+package activity;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:conf/sprint-context.xml" })
+public class MainTest {
+
+}
