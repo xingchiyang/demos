a53660479f2c410bdca5fde82beef0a451bb00c3
yangbian <yangbian@broada.com>
2017-01-04 09:38:00 +0800

    新增的文件

--- test/src/main/java/com/yb/test/spring/AnnotationBean.java
+++ test/src/main/java/com/yb/test/spring/AnnotationBean.java
@@ -2,9 +2,11 @@ package com.yb.test.spring;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
+@Lazy(false)
 public class AnnotationBean {
 	@PostConstruct
 	public void start() {
